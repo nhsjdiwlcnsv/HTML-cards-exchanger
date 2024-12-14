7f7ddef0f181106c0b9ac7f60400dae0ac9faac3
vlanuka <andreychuk04@mail.ru>
2024-12-14 17:30:43 +0300

    refactor: change project structure

--- api/app/index.js
+++ api/app/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL);
 
 module.exports = app;
