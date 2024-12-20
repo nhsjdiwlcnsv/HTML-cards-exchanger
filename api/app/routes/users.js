4a3f4d384149cb53ff15dfd7dc59a3f6fe09b48f
vlanuka <andreychuk04@mail.ru>
2024-12-20 18:27:37 +0300

    refactor: renamed middleware to uploadinf avatars

--- api/app/routes/users.js
+++ api/app/routes/users.js
@@ -1,27 +1,17 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
-const uploadAvatarMiddleware = require("../middleware/uploadAvatarMiddleware");
+const { uploadAvatar } = require("../middleware/uploadImagesMiddleware");
 const userController = require("../controllers/users");
 
 const router = express.Router();
 
 router.post("/login", userController.login);
-router.get(
-  "/profile",
-  authMiddleware,
-  uploadAvatarMiddleware,
-  userController.profile
-);
+router.get("/profile", authMiddleware, uploadAvatar, userController.profile);
 router.post("/logout", authMiddleware, userController.logout);
 
-router.post("/register", userController.create);
+router.post("/register", uploadAvatar, userController.create);
 router.get("/:id", userController.read);
-router.put(
-  "/:id",
-  authMiddleware,
-  uploadAvatarMiddleware,
-  userController.update
-);
+router.put("/:id", authMiddleware, uploadAvatar, userController.update);
 router.delete("/:id", authMiddleware, userController.remove);
 
 module.exports = router;
